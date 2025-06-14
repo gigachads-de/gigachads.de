d56881a0b0e1f38a85d0b280105075120199c1e2
gigachads-de <chad@gigachads.de>
2025-06-14 18:54:22 +0200

    Fixed .env

--- apps/api/src/configs/env/env.ts
+++ apps/api/src/configs/env/env.ts
@@ -1,15 +1,15 @@
 import z from "zod";
 
 export const schemaEnv = z.object({
-  TZ: z.string(),
-  NODE_ENV: z.string(),
+  TZ: z.string().default("Etc/UTC"),
+  NODE_ENV: z.string().default("production"),
   FASTIFY_HOST: z.string().ip().default("127.0.0.1"),
   FASTIFY_PORT: z.coerce.number().default(3000),
   REDIS_HOST: z.string(),
   REDIS_PASSWORD: z.string(),
   REDIS_PORT: z.coerce.number().default(6379),
   REDIS_IP_FAMILY: z.enum(["4", "6"]).pipe(z.coerce.number().default(4)),
-  LOG_ENABLED: z.boolean().default(true),
+  LOG_ENABLED: z.coerce.boolean().default(true),
   LOG_LEVEL: z.enum(["fatal", "error", "warn", "info", "debug", "trace"]).default("info")
 });
 
